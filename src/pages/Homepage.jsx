30a165ca8a93bf76acb1d6c6098ef8842f749f98
Michela <micheladellagatta1@gmail.com>
2025-11-18 18:16:01 +0100

    Add card to flex

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -5,7 +5,15 @@ export default function Homepage() {
     <>
       <Jumbotron />
       <div className="container my-5">
-        {/* Contenuti per cui non ho fantasia */}
+        <div class="card p-4 rounded-5 shadow">
+          <div class="card-body text-center">
+            <span className="gold-text">
+              <i className="bi bi-trophy-fill fs-1"></i>
+            </span>
+            <h2>8 years at the top</h2>
+            <p className="mb-0">TThe leading choice for our valued clients</p>
+          </div>
+        </div>
       </div>
     </>
   );
